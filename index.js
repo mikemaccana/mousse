0c3143130a11e986b08b42fb39fc0d941be78950
Mike MacCana <mike@certsimple.com>
2018-05-31 11:03:03 +0100

    ES8 rewrite Use template strings Use 'response' not 'res' USe 'fushHEaders()' not '.flush()' which is deprecated Just return rather than use 'else'

--- index.js
+++ index.js
@@ -3,21 +3,20 @@
 module.exports = function(sseErrorRetry, sseKeepAliveInterval){
 	'use strict';
 
-	var log = console.log.bind(console);
-
-	var channels = {}
-
-	// Server: How long to tell browsers to wait if the server doesn't respond to our request for messages.
-	var SSE_ERROR_RETRY = sseErrorRetry || 5 * 1000;
-
-	// See setupKeepAlives for more info
-	var SSE_KEEP_ALIVE_INTERVAL = sseKeepAliveInterval || 3 * 1000;
+	var log = console.log.bind(console),
+		// Key is channelID, value is object with messageID:Number and responses:Aarray 
+		channels = {},
+		// Server: How long to tell browsers to wait if the server doesn't respond to our request for messages.
+		SSE_ERROR_RETRY = sseErrorRetry || 5 * 1000,
+		// See setupKeepAlives for more info
+		SSE_KEEP_ALIVE_INTERVAL = sseKeepAliveInterval || 3 * 1000;
+	
 
 	// Helper used for both sendUpdate & setupKeepAlive
 	// Simply creates messages in SSE text format
 	// See http://www.html5rocks.com/en/tutorials/eventsource/basics/
 	// for more info on how these responses are constructed
-	var formatAndSendMessage = function(res, message, comment, messageID){
+	var formatAndSendMessage = function(response, message, comment, messageID){
 		var responseContents = {
 			id: messageID,
 			retry: SSE_ERROR_RETRY,
@@ -26,23 +25,23 @@ module.exports = function(sseErrorRetry, sseKeepAliveInterval){
 		};
 		var responseContentsFlattened = '';
 		if ( comment ) {
-			responseContentsFlattened += ':'+comment+'\n';
+			responseContentsFlattened += `:${comment}\n`;
 		}
 		for ( var key in responseContents ) {
-			responseContentsFlattened += key+': '+responseContents[key]+'\n';
+			responseContentsFlattened += `key: ${responseContents[key]}\n`;
 		}
-		res.write(responseContentsFlattened += '\n');
+		response.write(responseContentsFlattened += `\n`);
 
 		// Required when compression is enabled
-		res.flushHeaders()
+		response.flushHeaders()
 
 		// DO NOT end() the response - we'll send more messages in future.
 	};
 
 	// SSE requires 'keep alive' responses every so often otherwise browsers will disconnect
 	var setupKeepAlives = function(){
-		var sendKeepAlive = function(res, channel){
-			formatAndSendMessage(res, null, 'keepalive', channels[channel].messageID)
+		var sendKeepAlive = function(response, channel){
+			formatAndSendMessage(response, null, 'keepalive', channels[channel].messageID)
 			channels[channel].messageID += 1;
 		};
 		setInterval(function(){
@@ -54,43 +53,44 @@ module.exports = function(sseErrorRetry, sseKeepAliveInterval){
 		}, SSE_KEEP_ALIVE_INTERVAL)
 	};
 
-	// Add a client res to a channel, so the client will get all messages sent to that channel
-	var subscribeToChannel = function(channel, res){
+	// Add a client response to a channel, so the client will get all messages sent to that channel
+	var subscribeToChannel = function(channel, response){
 		// Send headers, and write data, but do not end the response - this is how SSE works
-		log('subscribeToChannel')
+		log(`Subscribing to channel ${channel}`)
 		// req.socket.setTimeout(Number.MAX_SAFE_INTEGER || Infinity);
 
 		// Start SSE response
-		res.writeHead(200, {
+		response.writeHead(200, {
 			'Content-Type': 'text/event-stream',
 			'Cache-Control': 'no-cache',
 			'Connection': 'keep-alive'
 		});
-		res.write('\n');
+
+		response.write(`\n`);
 
 		if ( channels[channel] ) {
-			log('adding response to channel', channel)
-			channels[channel].responses.push(res);
-		} else {
-			log('created new channel', channel)
-			channels[channel] = {
-				// Pending update responses.
-				responses: [res],
-				messageID: 0
-			}
+			log(`Adding response to channel ${channel}`)
+			channels[channel].responses.push(response);
+			return
+		} 
+		log(`Created new channel ${channel}`)
+		channels[channel] = {
+			// Pending update responses.
+			responses: [response],
+			messageID: 0
 		}
 	};
 
 	// Send message to each subscribed client on the channel
 	var sendUpdate = function(channel, message){
 		if ( channels[channel].responses ) {
-			channels[channel].responses.forEach(function(res) {
-				formatAndSendMessage(res, message, null, channels[channel].messageID)
+			channels[channel].responses.forEach(function(response) {
+				formatAndSendMessage(response, message, null, channels[channel].messageID)
 				channels[channel].messageID += 1;
 			});
-		} else {
-			log('No waiting clients on channel', channel)
-		}
+			return
+		} 
+		log('No waiting clients on channel', channel)
 	};
 
 	// Return a list of all channels with at least one subscriber
@@ -113,10 +113,10 @@ module.exports = function(sseErrorRetry, sseKeepAliveInterval){
 	setupKeepAlives();
 
 	return {
-		subscribeToChannel: subscribeToChannel,
-		sendUpdate: sendUpdate,
-		getActiveChannels: getActiveChannels,
-		sendToActiveChannels: sendToActiveChannels
+		subscribeToChannel,
+		sendUpdate,
+		getActiveChannels,
+		sendToActiveChannels
 	};
 }
 
